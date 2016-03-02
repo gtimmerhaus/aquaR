6f8622742e466c91e4b5d5cff202700b1329fe50
Gerrit Timmerhaus <gerrit.timmerhaus@gmail.com>
2016-03-02 12:52:35 +0100

    breakpoint arrhenius bug fixing

--- R/breakPointsArrhenius.R
+++ R/breakPointsArrhenius.R
@@ -16,62 +16,69 @@
 #' @examples
 #' breakPointsArrhenius(x=data[,-1], group=groups)
 
+x <- normalized[,11:18]
+group <- normalized$group
+col <- cols[1:3]
+pch <- 16
+error.bars <- T
 
 breakPointsArrhenius <- function(x, group, col="black", marks=T, obs=3, error.bars=T,
                                  ylim=c(4,5.2), pch=16, legend=T, xlab="Temperature (°C)", ylab="ln(HR max)") {
-  arr.vals <- rev(log(x))
-  m <- aggregate(arr.vals, by = list(group), mean, na.rm=T)
-  e <- aggregate(arr.vals, by = list(group), sterr)
-  
-  #chech for sufficient colors:
-  if(length(unique(group)) > length(col)){
-      warning("not enough colors in vector col. Using greys instead")
-      col <- paste0("grey", seq(0,70, length.out = length(unique(group))))
-  }
-  if (length(pch)<nrow(m)) {pch<-rep(pch, nrow(m))} #supply enough pch's for the loop
-  
-  labels <- gsub("X","",colnames(arr.vals),perl=T)
-  
-  plot(as.numeric(m[1,-1]), ylim=ylim, type="p", pch=pch[1], 
-  col=col[1], xaxt="n", xlab=xlab, ylab=ylab, las=2)
-  if (error.bars) {arrows(1:ncol(m), as.numeric(m[1,-1]-e[1,-1]), 1:ncol(m), as.numeric(m[1,-1]+e[1,-1]), angle = 90, length = 0.05, code = 3)}
-  if (nrow(m)>1){
-    for (i in 2:nrow(m)){
-      points(as.numeric(m[i,-1]), type="p", pch=pch[i], col=col[i])
-      if (error.bars) {arrows(1:ncol(m), as.numeric(m[i,-1]-e[i,-1]), 1:ncol(m), as.numeric(m[i,-1]+e[i,-1]), angle = 90, length = 0.05, code = 3)}
+    arr.vals <- rev(log(x))
+    m <- aggregate(arr.vals, by = list(group), mean, na.rm=T)
+    e <- aggregate(arr.vals, by = list(group), sterr)
+    
+    #chech for sufficient colors:
+    if(length(unique(group)) > length(col)){
+        warning("not enough colors in vector col. Using greys instead")
+        col <- paste0("grey", seq(0,70, length.out = length(unique(group))))
     }
-  }
-  axis(1, at=1:ncol(arr.vals), labels=labels)
-  
-  #breakpoint calculation:
-  res <- matrix(rep(NA,nrow(m)*2),ncol=2)
-  colnames(res) <- c("bestCor", "column")
-  temp <- m[,-1] #all columns minus the group column
-  for(i in 1:nrow(temp)){
-    bestCor <- 1
-    lowestP <- 1
-    for(ii in (ncol(temp)-2):2){ #find the best correlation or lowest p-value (depending on method = "correl" | "pvalue")
-      if(cor(as.numeric(m[i,ii:ncol(temp)]),ii:ncol(temp), use="complete.obs") < bestCor){#find the best correlation from last point to ii
-        bestCor <- cor(as.numeric(temp[i,ii:ncol(temp)]),ii:ncol(temp), use="complete.obs")
-        res[i,1] <- bestCor #save best correlation
-        res[i,2] <- ii  #remember column with best correlation
-      }
+    if (length(pch)<nrow(m)) {pch<-rep(pch, nrow(m))} #supply enough pch's for the loop
+    
+    labels <- gsub("X","",colnames(arr.vals),perl=T)
+    
+    plot(as.numeric(m[1,-1]), ylim=ylim, type="p", pch=pch[1], 
+         col=col[1], xaxt="n", xlab=xlab, ylab=ylab, las=2)
+    if (error.bars) {arrows(1:ncol(m), as.numeric(m[1,-1]-e[1,-1]), 1:ncol(m), as.numeric(m[1,-1]+e[1,-1]), angle = 90, length = 0.05, code = 3)}
+    if (nrow(m)>1){
+        for (i in 2:nrow(m)){
+            points(as.numeric(m[i,-1]), type="p", pch=pch[i], col=col[i])
+            if (error.bars) {arrows(1:ncol(m), as.numeric(m[i,-1]-e[i,-1]), 1:ncol(m), as.numeric(m[i,-1]+e[i,-1]), angle = 90, length = 0.05, code = 3)}
+        }
     }
-    #add linear models for best correlation (by adding point after point):
-    linInc <- lm(as.numeric(temp[i,ncol(temp):res[i,2]])~c(ncol(temp):res[i,2])) #lm() for linear increase
-    beyInc <- lm(as.numeric(temp[i,(res[i,2]-1):(res[i,2]-(obs+1))])~c((res[i,2]-1):(res[i,2]-(obs+1)))) #lm() for beyond breakpoint observations (n=obs)
-    #calculate meeting point:
-    xpos <- (beyInc$coefficients[1]-linInc$coefficients[1])/(linInc$coefficients[2]-beyInc$coefficients[2])
-    ypos <- linInc$coefficients[1]+linInc$coefficients[2]*xpos
-    #draw linear lines:
-    points(c(xpos, ncol(temp)), c(ypos, linInc$coefficients[1]+linInc$coefficients[2]*ncol(temp)), col=col[i], type="l")
-    points(c(xpos, floor(xpos)-obs), c(ypos, beyInc$coefficients[1]+beyInc$coefficients[2]*(floor(xpos)-obs)), col=col[i], type="l")
-    #draw indicator line:
-    points(rep(xpos,2), c(ypos,0), type="l", lty=2, col=col[i])
-    #print calculated ABT:
-    print(paste0(m$Group.1[i],": ", round(as.numeric(labels[floor(xpos)]) - xpos %% 1,3)))
-  }
-  
-  if (legend) {legend("topright", legend=m$Group.1, pch=pch, col=col, lwd=1)}
+    axis(1, at=1:ncol(arr.vals), labels=labels)
+    
+    #breakpoint calculation:
+    res <- matrix(rep(NA,nrow(m)*2),ncol=2)
+    colnames(res) <- c("bestCor", "column")
+    temp <- m[,-1] #all columns minus the group column
+    for(i in 1:nrow(temp)){
+        bestCor <- 1
+        lowestP <- 1
+        for(ii in (ncol(temp)-2):2){ #find the best correlation 
+            if(cor(as.numeric(temp[i,ii:ncol(temp)]), ii:ncol(temp), use="complete.obs") < bestCor){#find the best correlation from last point to ii
+                bestCor <- cor(as.numeric(temp[i,ii:ncol(temp)]), ii:ncol(temp), use="complete.obs")
+                res[i,1] <- bestCor #save best correlation
+                res[i,2] <- ii  #remember column with best correlation
+            }
+        }
+        #add linear models for best correlation (by adding point after point):
+        linInc <- lm(as.numeric(temp[i,ncol(temp):res[i,2]])~c(ncol(temp):res[i,2])) #lm() for linear increase
+        model.x <- c((res[i,2]-1):(res[i,2]-(obs+1))) #x-values for the following linear model 
+        beyInc <- lm(as.numeric(temp[i,(res[i,2]-1):(res[i,2]-(obs+1))])~model.x[model.x!=0]) #lm() for beyond breakpoint observations (n=obs), if there are 0s in points.x, there are removed
+        #calculate meeting point:
+        xpos <- (beyInc$coefficients[1]-linInc$coefficients[1])/(linInc$coefficients[2]-beyInc$coefficients[2])
+        ypos <- linInc$coefficients[1]+linInc$coefficients[2]*xpos
+        #draw linear lines:
+        points(c(xpos, ncol(temp)), c(ypos, linInc$coefficients[1]+linInc$coefficients[2]*ncol(temp)), col=col[i], type="l")
+        points(c(xpos, floor(xpos)-obs), c(ypos, beyInc$coefficients[1]+beyInc$coefficients[2]*(floor(xpos)-obs)), 
+               col=col[i], type="l")
+        #draw indicator line:
+        points(rep(xpos,2), c(ypos,0), type="l", lty=2, col=col[i])
+        #print calculated ABT:
+        print(paste0(m$Group.1[i],": ", round(as.numeric(labels[floor(xpos)]) - xpos %% 1,3)))
+    }
+    
+    if (legend) {legend("topright", legend=m$Group.1, pch=pch, col=col, lwd=1)}
 }
 
