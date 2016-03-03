d6a6f28c254c1692c2474a0c673558cb06d7bbec
Gerrit Timmerhaus <gerrit.timmerhaus@gmail.com>
2016-03-03 15:04:56 +0100

    breakpoint arrhenius bug fixing

--- R/breakPointsArrhenius.R
+++ R/breakPointsArrhenius.R
@@ -16,17 +16,18 @@
 #' @examples
 #' breakPointsArrhenius(x=data[,-1], group=groups)
 
-x <- normalized[,11:18]
-group <- normalized$group
-col <- cols[1:3]
-pch <- 16
-error.bars <- T
+x<- normalized_w10[,11:18]
+group <- normalized_w10$group
+col <- cols[c(1,5,9)]
+
 
 breakPointsArrhenius <- function(x, group, col="black", marks=T, obs=3, error.bars=T,
-                                 ylim=c(4,5.2), pch=16, legend=T, xlab="Temperature (°C)", ylab="ln(HR max)") {
+                                 ylim=c(4,5.2), pch=16, legend=T, xlab="Temperature (degrees C)", ylab="ln(HR max)") {
     arr.vals <- rev(log(x))
     m <- aggregate(arr.vals, by = list(group), mean, na.rm=T)
     e <- aggregate(arr.vals, by = list(group), sterr)
+    
+    if(obs<3){stop("obs needs to be bigger than 2")} #correlation with two or less values not meaninful
     
     #chech for sufficient colors:
     if(length(unique(group)) > length(col)){
@@ -51,28 +52,25 @@ breakPointsArrhenius <- function(x, group, col="black", marks=T, obs=3, error.ba
     #breakpoint calculation:
     res <- matrix(rep(NA,nrow(m)*2),ncol=2)
     colnames(res) <- c("bestCor", "column")
-    temp <- m[,-1] #all columns minus the group column
-    for(i in 1:nrow(temp)){
-        bestCor <- 1
-        lowestP <- 1
-        for(ii in (ncol(temp)-2):2){ #find the best correlation 
+    temp <- m[,-1] #remove group column
+    for(i in 1:nrow(temp)){ #one iteration per group
+        bestCor <- 1 #init a bad correlation value (target will be close to -1)
+        for(ii in (ncol(temp)-obs+1):2){ #find the best correlation:
             if(cor(as.numeric(temp[i,ii:ncol(temp)]), ii:ncol(temp), use="complete.obs") < bestCor){#find the best correlation from last point to ii
                 bestCor <- cor(as.numeric(temp[i,ii:ncol(temp)]), ii:ncol(temp), use="complete.obs")
                 res[i,1] <- bestCor #save best correlation
                 res[i,2] <- ii  #remember column with best correlation
             }
         }
-        #add linear models for best correlation (by adding point after point):
+        #add linear models for best correlation:
         linInc <- lm(as.numeric(temp[i,ncol(temp):res[i,2]])~c(ncol(temp):res[i,2])) #lm() for linear increase
-        model.x <- c((res[i,2]-1):(res[i,2]-(obs+1))) #x-values for the following linear model 
-        beyInc <- lm(as.numeric(temp[i,(res[i,2]-1):(res[i,2]-(obs+1))])~model.x[model.x!=0]) #lm() for beyond breakpoint observations (n=obs), if there are 0s in points.x, there are removed
+        beyInc <- lm(as.numeric(temp[i,(res[i,2]-1):1])~c((res[i,2]-1):1)) #lm() for beyond breakpoint observations
         #calculate meeting point:
         xpos <- (beyInc$coefficients[1]-linInc$coefficients[1])/(linInc$coefficients[2]-beyInc$coefficients[2])
         ypos <- linInc$coefficients[1]+linInc$coefficients[2]*xpos
         #draw linear lines:
         points(c(xpos, ncol(temp)), c(ypos, linInc$coefficients[1]+linInc$coefficients[2]*ncol(temp)), col=col[i], type="l")
-        points(c(xpos, floor(xpos)-obs), c(ypos, beyInc$coefficients[1]+beyInc$coefficients[2]*(floor(xpos)-obs)), 
-               col=col[i], type="l")
+        points(c(xpos, 1), c(ypos, beyInc$coefficients[1]+beyInc$coefficients[2]*1), col=col[i], type="l")
         #draw indicator line:
         points(rep(xpos,2), c(ypos,0), type="l", lty=2, col=col[i])
         #print calculated ABT:
@@ -81,4 +79,5 @@ breakPointsArrhenius <- function(x, group, col="black", marks=T, obs=3, error.ba
     
     if (legend) {legend("topright", legend=m$Group.1, pch=pch, col=col, lwd=1)}
 }
+
 
